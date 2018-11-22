b55054ddd85decbc123f743801cffb480e05af39
louisbaggins <44960191+louisbaggins@users.noreply.github.com>
2018-11-22 19:44:48 -0200

    Update media.vhd

--- media.vhd
+++ media.vhd
@@ -9,6 +9,6 @@ end media;
 
 architecture comportamental of media is
 begin
-    MA <= (x/3) + (y/3) + (z/3) ;
-	 MH <= 3/(1/X + 1/Y + 1/Z);
+    MA <= conv_std_logic_vector((conv_integer(x)/3) + (conv_integer(y)/3) + (conv_integer(z)/3), 16);
+    MH <= conv_std_logic_vector(3/(1/conv_integer(x) + 1/conv_integer(y) + 1/conv_integer(z), 16);
 end comportamental;
